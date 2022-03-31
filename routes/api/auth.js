a690c42ea6633a9331ffc828c916597a1fc95122
Giorgi <gkublashvili@unisens.ge>
2022-03-31 15:02:41 +0400

    added authenitcation middlware, to get authentiaced user you need to provide token that was given to you when you created the user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const router = express.Router()
-
+const User = require('../../models/User')
+const auth = require('../../middleware/auth')
 // @route Get api/users
 // @desc Test route
 // @acces Public
-router.get('/', (req, res)=>{
-    res.send("auth route")
+router.get('/', auth, async (req, res)=>{
+    try{
+        console.log('made it here')
+        //we assing value to req.user inside auth.js
+        const user = await User.findById(req.user.id).select('-password')
+        res.json(user)
+    }catch(err){
+        console.error(err)
+        res.status(500).send('server erorr')
+    }
 })
 
 module.exports = router

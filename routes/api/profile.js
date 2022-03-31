4530fc85b26f6a4e0357aae68ac5d96837960b41
Giorgi <gkublashvili@unisens.ge>
2022-03-31 23:45:20 +0400

    16. Get Current User Profile
    added logic to get profile for logged in user, route is private since user should be logged in to get it's profile info, user has to provide token it recieved when logging in

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../../middleware/auth')
+const User = require('../../models/User')
+const Profile = require('../../models/Profile')
 
-// @route Get api/users
-// @desc Test route
-// @acces Public
-router.get('/', (req, res)=>{
-    res.send("Profile route")
+// @route Get api/profile/me
+// @desc get current users profile
+// @acces Private
+router.get('/me', auth, async (req, res)=>{
+    try{
+        const profile = await Profile.findOne({user:req.user.id}).populate('user', ['name', 'avatar'])
+        
+        if(!profile){
+            return res.status(400).json({msg:"there is no profile for this user"})
+        }
+
+        res.json(profile)
+    }catch(err){
+        console.error(err)
+        res.status(500).send('server error')
+
+    }
+
 })
 
 module.exports = router
